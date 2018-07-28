b6aecb75915d1430328e6139d21aa06dcb70312b
jslyne <jslyn.vip@gmail.com>
2018-07-28 04:53:51 -0500

    traer atos eliminados

--- src/js/firebase.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: "AIzaSyCsWWyT_6yQo2aTelsx9GaH8B39VWD98x8",
-    authDomain: "brainyplaylab.firebaseapp.com",
-    databaseURL: "https://brainyplaylab.firebaseio.com",
-    projectId: "brainyplaylab",
-    storageBucket: "brainyplaylab.appspot.com",
-    messagingSenderId: "60588851338"
-};
-firebase.initializeApp(config);
