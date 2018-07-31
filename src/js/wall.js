741a1f1a7e41fab9da41a217f70bb0ab384d3c3c
jslyne <jslyn.vip@gmail.com>
2018-07-31 14:25:13 -0500

    implementando nuevo diseño, guarda, edita y elimina

--- src/js/wall.js
+++ src/js/wall.js
@@ -3,18 +3,23 @@ const btnLogout = document.getElementById('btn-logout'),
     post = document.getElementById('post'),
     dataBase = document.getElementById('data-base'),
     postArea = document.getElementById('posts-area'),
-    userName = document.getElementById('user-name'),
-    userImage = document.getElementById('user-image'),
-    userEmail = document.getElementById('user-email');
+    nomUser = document.getElementById('user-name'),
+    imgUser = document.getElementById('user-image'),
+    emailUser = document.getElementById('user-email');
 
 btnLogout.addEventListener('click', () => {
     logoutWithFirebase();
 });
 
 btnPublicar.addEventListener('click', () => {
-    createNewPost();
+    if (post.value.length !== 0 && post.value.trim() !== '') {
+        createNewPost();
+    }
+    else {
+        alert('Escribe un comentario');
+    }
 });
 
-const reload_page = () => {
-    window.location.reload();
-};
+// const reload_page = () => {
+//     window.location.reload();
+// };
