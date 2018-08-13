cc2b022db50e2523d40f48c1b3db7ff3bcf4cb4b
jslyne <jslyn.vip@gmail.com>
2018-08-13 15:27:13 -0500

    Foto del Usuario

--- src/js/splash.js
+++ src/js/splash.js
@@ -6,7 +6,7 @@
     function frame() {
         if (loading === 100) {
             clearInterval(id);
-            window.open("profile.html", "_self");
+            window.open("perfil.html", "_self");
         }
         else {
             loading = loading + 1;
